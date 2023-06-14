07cbc266c570cc84c24aa994c9c55abf2bb836a6
HarshKakran <harshkakran05@gmail.com>
2023-06-14 16:37:51 +0530

    Moving to Flask rather than Django

--- fitJoe/bot/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-import os
-
-import openai
-
-from dotenv import load_dotenv, find_dotenv
-_ = load_dotenv(find_dotenv())
-openai.api_key = os.getenv('GPT_API_KEY')
