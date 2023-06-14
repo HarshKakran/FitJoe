47a325c1ec4b51aed9f7b1ed4e728ee83c054c18
HarshKakran <harshkakran05@gmail.com>
2023-06-14 15:52:23 +0530

    Added environment variables

--- fitJoe/bot/views.py
+++ fitJoe/bot/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+import os
 
-# Create your views here.
+import openai
+
+from dotenv import load_dotenv, find_dotenv
+_ = load_dotenv(find_dotenv())
+openai.api_key = os.getenv('GPT_API_KEY')
